3ee09ff36bc394b1178d58c976b143c02d47628b
Arman Bhimani <Armanbhimani@gmail.com>
2019-06-14 20:55:14 -0500

    Removed helmet

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ const morganOption = (NODE_ENV === 'production')
 
 app.use(morgan(morganOption))
 app.use(cors())
-// app.use(helmet())
+app.use(helmet())
 
 app.use('/api/posts', postsRouter)
 app.use('/api/users', usersRouter)
@@ -24,6 +24,26 @@ app.use('/api/auth', authRouter)
 
 app.get('/', (req, res) => {
     res.send('Hello, world!')
+})
+
+app.get('/manifest.json', (req, res) => {
+    res.json({
+        "short_name": "Dream Dogs",
+        "manifest_version": 2,
+        "version": "one",
+        "name": "Dream Dogs App",
+        "icons": [
+          {
+            "src": "dog.ico",
+            "sizes": "64x64 32x32 24x24 16x16",
+            "type": "image/x-icon"
+          }
+        ],
+        "start_url": "./index.html",
+        "display": "standalone",
+        "theme_color": "#000000",
+        "background_color": "#ffffff"
+      })
 })
 
 app.use(function errorHandler(error, req, res, next) {
