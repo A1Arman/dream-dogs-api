50a6303531727eb7a98e8d9bfda387d043b866f0
Arman Bhimani <Armanbhimani@gmail.com>
2019-06-14 20:51:20 -0500

    Removed helmet

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ const morganOption = (NODE_ENV === 'production')
 
 app.use(morgan(morganOption))
 app.use(cors())
-app.use(helmet())
+// app.use(helmet())
 
 app.use('/api/posts', postsRouter)
 app.use('/api/users', usersRouter)
